075ed5d30e404b2d3ac048160535c1276c5b104f
Spiros Konomis <spiros.konomis@ibm.com>
2023-04-20 12:27:24 +0300

    admin navbar redirect fix

--- adminNavbar.php
+++ adminNavbar.php
@@ -59,7 +59,7 @@ session_start();
 
 <!-- ΝΕΟ ΔΙΟΡΘΩΜΕΝΟ NAVBAR - RESPONSIVE ΓΙΑ ΚΙΝΗΤΑ  -->
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-	<a class="navbar-brand" href="index.php">Admin Hotel <i class="bi bi-code-slash"></i></a>
+	<a class="navbar-brand" href="admin.php">Admin Hotel <i class="bi bi-code-slash"></i></a>
 	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
 		aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 		<span class="navbar-toggler-icon"></span>
